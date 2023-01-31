828070231e3b0767d0f86bf449b37ecb18b2af56
Rahul Kumar <r1303yadav@gmail.com>
2023-01-31 16:03:06 +0530

    code is done

---
+++ SpiralMatrix.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class SpiralMatrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        printMatrix(createSpiralMatrix(n));     // both function call
+        sc.close();
+    }
+
+    public static int[][] createSpiralMatrix(int n) {
+        int val = 1, topRow = 0, bottomRow = n - 1, leftCol = 0, rightCol = n - 1;
+        int[][] matrix = new int[n][n];
+        while (val <= n * n) {
+            // fill top row from left to right
+            for (int i = leftCol; i <= rightCol; i++) {
+                matrix[topRow][i] = val++;
+            }
+            topRow++;
+            // fill right column from top to bottom
+            for (int i = topRow; i <= bottomRow; i++) {
+                matrix[i][rightCol] = val++;
+            }
+            rightCol--;
+            // fill bottom row from right to left
+            if (topRow <= bottomRow) {
+                for (int i = rightCol; i >= leftCol; i--) {
+                    matrix[bottomRow][i] = val++;
+                }
+                bottomRow--;
+            }
+            // fill left column from bottom to top
+            if (leftCol <= rightCol) {
+                for (int i = bottomRow; i >= topRow; i--) {
+                    matrix[i][leftCol] = val++;
+                }
+                leftCol++;
+            }
+        }
+        return matrix;
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int[] arr : matrix) {
+            for (int j : arr) {
+                System.out.print(j + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
